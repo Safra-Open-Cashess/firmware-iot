0f7eaee154ef78f6aec89ac8a789c01f75c8c395
unknown <wilfaustino@gmail.com>
2022-04-02 17:05:16 -0300

    Added keyboard

--- src/open-cashless-firmware-iot.ino
+++ src/open-cashless-firmware-iot.ino
@@ -1,5 +1,6 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
 
 //Display
 LiquidCrystal_I2C lcd(0x27, 2,1,0,4,5,6,7,3, POSITIVE);
@@ -7,6 +8,20 @@ LiquidCrystal_I2C lcd(0x27, 2,1,0,4,5,6,7,3, POSITIVE);
 //Constants
 const int buzzer = 17;
 const int rfidPin = 14;
+
+//Keypad
+const byte LINHAS = 4;
+const byte COLUNAS = 3;
+byte PinosLinhas[LINHAS] = {0, 1, 2, 3};
+byte PinosColunas[COLUNAS] = {4, 5, 6};
+char matriz_teclas[LINHAS][COLUNAS] = {
+    {'1','2','3'},
+    {'4','5','6'},
+    {'7','8','9'},
+    {'*','0','#'}
+};
+
+Keypad key_board = Keypad(makeKeymap(matriz_teclas), PinosLinhas, PinosColunas, LINHAS, COLUNAS);
 
 void setup() {
 
@@ -32,6 +47,37 @@ void setup() {
 }
 
 void loop() {
+
+    //Keyboard
+    String valor = "";
+    while(true){
+        char tecla_pressionada = key_board.getKey();
+        if (tecla_pressionada){
+            buzzerTone(1, 1000, 100);
+            Serial.print("Tecla pressionada : ");
+            Serial.println(tecla_pressionada);
+
+            if(tecla_pressionada == '*'){
+                tecla_pressionada = '.';
+            } else if(tecla_pressionada == '#'){
+                break;
+            }
+            
+            valor = valor + String(tecla_pressionada);
+            Serial.println(valor);
+            
+            lcd.setCursor(1, 1);
+            lcd.print(valor + "               ");
+        }
+    }
+
+    buzzerTone(3, 2000, 100);
+
+    lcd.setCursor(0, 1);
+    lcd.print("Aprox. pulseira");
+    delay(400);
+
+    //Read RFID
     
 }
 
